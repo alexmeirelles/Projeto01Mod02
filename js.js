2adb00af8dd014f59adbcdbb8880cbcdb783b1c3
alexmeirelles <alemeirelles@msn.com>
2022-03-10 23:32:57 -0300

    working js

--- js.js
+++ js.js
@@ -2,7 +2,7 @@
 const elementolivros = document.getElementById("livroslidos");
 const elementonivel = document.querySelector("#nivel");
 const elementoImg = document.querySelector("#imagem");
-let elementoBtn = document.querySelector("#alterar");
+let elementoBtn = document.querySelector("#botao");
 
 elementoBtn.addEventListener("click", () => {
   if (elementoBtn.value == "primeiro") {
@@ -12,18 +12,13 @@ elementoBtn.addEventListener("click", () => {
     elementoBtn.value = "segundo";
 } else if (elementoBtn.value == "segundo") {
     elementoImg.src = "bill.jpeg";
+    elementolivros.innerText = "Até Livros Lidos : 8";
+    elementonivel.innerText = "Nível: Bill Gates";
+    elementoBtn.value = "terceiro";
+} else {
+    elementoImg.src = "warren.jpeg";
     elementolivros.innerText = "Até Livros Lidos : 12";
     elementonivel.innerText = "Nível: Warren Buffet";
-    elementoBtn.value = "terceiro";
-} else if (elementoBtn.value == "terceiro") {
-    elementoImg.src = "warren.jpeg";
-    elementolivros.innerText = "apanhando";
-    elementonivel.innerText = "sofrendo";
-    elementoBtn.value = "quarto";
-  } else {
-    elementoImg.src = "prep2.png";
-    elementolivros.innerText = "Seya Preparando";
-    elementonivel.innerText = "Preparando para batalhar";
     elementoBtn.value = "primeiro";
-  }
+  } 
 });
